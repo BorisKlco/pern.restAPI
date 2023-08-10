7401b99e78b8d2c481139d2661b8474c7318d39c
BorisKlco <klco@pm.me>
2023-08-10 16:17:51 +0200

    delete option

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,11 +1,47 @@
 import express from "express";
 import dbConnect from "../db/connect";
-import { UserModelType } from "types";
 
 export async function getAllUsers(req: express.Request, res: express.Response) {
   const pool = dbConnect();
 
-  const fetchUsers: UserModelType = await pool.query("SELECT * from users");
+  const fetchUsers = await pool.query("SELECT * from users");
 
   return res.status(200).json(fetchUsers.rows);
+}
+
+export async function deleteYourself(
+  req: express.Request,
+  res: express.Response
+) {
+  const getUserToken = req.cookies.AYAYA;
+  if (!getUserToken) {
+    return res.sendStatus(403);
+  }
+
+  const pool = dbConnect();
+  const deleteMyself = await pool.query(
+    "DELETE FROM users WHERE sessiontoken = $1",
+    [getUserToken]
+  );
+
+  if (deleteMyself.rowCount != 0) {
+    return res.status(200).send("User deleted by himself...");
+  }
+
+  return res.status(500).send("Internal Error: User wasn't deleted...");
+}
+
+export async function deleteUser(req: express.Request, res: express.Response) {
+  const pool = dbConnect();
+  const { id } = req.params;
+
+  const deleteUserById = await pool.query("DELETE FROM users WHERE id = $1", [
+    id,
+  ]);
+
+  if (deleteUserById.rowCount != 0) {
+    return res.status(200).send(`User by ID ${id} was deleted...`);
+  }
+
+  return res.status(404).send("User ID doesnt exist...");
 }
--- src/router/getUsersRouter.ts
+++ src/router/getUsersRouter.ts
@@ -1,9 +1,11 @@
 import express from "express";
 
-import { getAllUsers } from "../controllers/users";
+import { getAllUsers, deleteYourself, deleteUser } from "../controllers/users";
 import { isAuth } from "../mw";
 
 export default function (router: express.Router) {
   console.log("'/users' Routes loaded...");
   router.get("/users", isAuth, getAllUsers);
+  router.post("/delete", isAuth, deleteYourself);
+  router.post("/delete/:id", isAuth, deleteUser);
 }
